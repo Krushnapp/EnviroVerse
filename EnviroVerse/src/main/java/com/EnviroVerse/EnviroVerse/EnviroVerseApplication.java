1e34a72778c9a7c4645ccc72337f885b69244ffe
Krushnapp <krush15699@gamail.com>
2023-12-10 12:45:05 +0530

    1

---
+++ EnviroVerse/src/main/java/com/EnviroVerse/EnviroVerse/EnviroVerseApplication.java
@@ -0,0 +1,13 @@
+package com.EnviroVerse.EnviroVerse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnviroVerseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnviroVerseApplication.class, args);
+	}
+
+}
